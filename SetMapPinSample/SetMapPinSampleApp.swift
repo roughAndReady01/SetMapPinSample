60e629234fca95c16c09de3679523bead5c69deb
Haruki Hanagata <haruzo28@icloud.com>
2023-01-15 07:48:48 +0900

    Initial Commit

---
+++ SetMapPinSample/SetMapPinSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetMapPinSampleApp.swift
+//  SetMapPinSample
+//
+//  Created by 花形春輝 on 2023/01/15.
+//
+
+import SwiftUI
+
+@main
+struct SetMapPinSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
